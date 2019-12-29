73f6ed8256a3bb409fa87be6db5764fc9b6f4611
beemtny <31330212+beemtny@users.noreply.github.com>
2019-12-30 01:57:33 +0700

    deploy

---
+++ precache-manifest.f415ffce18fc04bc1a14067daff2be24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67a9bf574f8a62641cc30b18bd784aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43e4c4514af369267554",
+    "url": "/static/css/main.939dbfbb.chunk.css"
+  },
+  {
+    "revision": "b48fb99b3a64f7e5e15d",
+    "url": "/static/js/2.04f8bbfc.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.04f8bbfc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43e4c4514af369267554",
+    "url": "/static/js/main.cf04cbfd.chunk.js"
+  },
+  {
+    "revision": "1f8967ab231f68366bfb",
+    "url": "/static/js/runtime-main.27fd7fd5.js"
+  }
+]);
